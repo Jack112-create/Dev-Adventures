7a27bbfe3a22c0593154c76603d5deeee16685dc
Jack Conroy <jackconroy777@gmail.com>
2021-11-22 17:21:37 +0000

    Add html questions to questions.py

---
+++ adventure/questions.py
@@ -0,0 +1,27 @@
+html_questions = [
+    {
+        'question': 'What does HTML stand for?:',
+        'answers': ['Hyper Text Meta Language', 'Hyper Text Markup Language', 'Hyperlinks Mark-up Language'],
+        'correct': 'Hyper Text Markup Language'
+    },
+    {
+        'question': 'Is <img> a HTML element?:',
+        'answers': ['Yes', 'No'],
+        'correct': 'Yes'
+    },
+    {
+        'question': 'Choose the correct HTML element for the largest heading:',
+        'answers': ['<h1>', '<head>', '<h6>'],
+        'correct': '<h1>'
+    },
+    {
+        'question': 'What is the correct HTML element for inserting a line break?:',
+        'answers': ['<br>', '<break>', '<b>'],
+        'correct': '<br>'
+    },
+    {
+        'question': 'What is the correct HTML for adding a font size?:',
+        'answers': ['<body style="font-size: 20px;">', '<body style="font: 20px">', '<body style="font-size: 20px">'],
+        'correct': '<body style="font-size: 20px;">'
+    }
+]
