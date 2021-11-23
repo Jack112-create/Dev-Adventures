83e79a4d5b6558faf5d4b82fa0ee18e7bc602296
Jack Conroy <jackconroy777@gmail.com>
2021-11-23 21:27:32 +0000

    Add show stats method to player class

--- adventure/player.py
+++ adventure/player.py
@@ -14,3 +14,18 @@ class Player:
         self.confidence = 100
         self.skills = []
         self.score = 0
+
+    def show_stats(self):
+        print(f"""
+{self.name.upper()} THE DEVELOPER!
+Here are your stats:
+Name: {self.name}
+----------------------
+Confidence: {self.confidence}
+----------------------
+Skills: {self.skills}
+----------------------
+Job: {self.job}
+----------------------
+Score: {self.score}
+""")
