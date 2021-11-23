c6ee8c5b84bd771f2cd9002f043d5f3c5b680b1e
Jack Conroy <jackconroy777@gmail.com>
2021-11-23 20:56:47 +0000

    Add room_task method to Room Class

--- adventure/rooms.py
+++ adventure/rooms.py
@@ -13,3 +13,14 @@ class Room:
     def __init__(self, room, room_questions):
         self.room = room
         self.room_questions = room_questions
+
+    def room_task(self):
+        clear_terminal()
+        # Instructions on how to play.
+        print(f"""
+    Welcome to the {self.room} quiz!
+    In order to move on with your learning you must put your knowledge of {self.room} to the test!
+    You will be asked 5 questions:
+    - Get a question right and your score will increase.
+    - Get a question wrong and watch both your confidence level and score go down!
+    """)
