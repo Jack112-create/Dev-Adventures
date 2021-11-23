20eb293d8fd40570a1b8487af0b0454130e1855a
Jack Conroy <jackconroy777@gmail.com>
2021-11-23 20:47:26 +0000

    Add Room class with initializer

--- adventure/rooms.py
+++ adventure/rooms.py
@@ -1,3 +1,15 @@
 from functions import clear_terminal
 import random
 import time
+
+
+class Room:
+    """
+    Creates an instance of a quiz room,
+    passing in the language and questions
+    about the language.
+    """
+    
+    def __init__(self, room, room_questions):
+        self.room = room
+        self.room_questions = room_questions
