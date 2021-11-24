40f4b435ab1d090f167999d308c0c7b1606b1690
Jack Conroy <jackconroy777@gmail.com>
2021-11-24 11:05:11 +0000

    Add imports to game.py

---
+++ adventure/game.py
@@ -0,0 +1,8 @@
+import time
+from functions import clear_terminal, game_over
+from player import Player
+from rooms import Room
+from questions import (
+    html_questions, css_questions,
+    javascript_questions, python_questions
+)
