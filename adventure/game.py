e691fb3f64940d35a6a2b621fcb3ec1d936b997c
Jack Conroy <jackconroy777@gmail.com>
2021-11-24 11:09:17 +0000

    Add player class instance variables

--- adventure/game.py
+++ adventure/game.py
@@ -13,3 +13,11 @@ class Game:
     Game instance brings player through each room,
     giving the player choices to make.
     """
+
+    def __init__(self, user):
+        self.user = Player(user)
+        self.rooms = ['HTML', 'CSS', 'JavaScript', 'Python']
+        self.html_room = Room('HTML', html_questions)
+        self.css_room = Room('CSS', css_questions)
+        self.javascript_room = Room('JavaScript', javascript_questions)
+        self.python_room = Room('Python', python_questions)
